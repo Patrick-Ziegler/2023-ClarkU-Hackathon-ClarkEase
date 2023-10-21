10461db17783b685aceb2f53b8d6f0475f38ffde
Patrick-Ziegler <104460499+Patrick-Ziegler@users.noreply.github.com>
2023-10-21 16:53:16 -0400

    Created buildingData model
    Contains the following fields
    buildingName = models.CharField(max_length=64)
        hasElevator = models.BooleanField()
        isElevatorWorking = models.BooleanField()
        hasRamps = models.BooleanField()
        motorizedEntrances = models.PositiveSmallIntegerField()
        totalEntrances = models.PositiveSmallIntegerField()

--- clarkEase/pages/models.py
+++ clarkEase/pages/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class buildingData(models.Model):
+    buildingName = models.CharField(max_length=64)
+    hasElevator = models.BooleanField()
+    isElevatorWorking = models.BooleanField()
+    hasRamps = models.BooleanField()
+    motorizedEntrances = models.PositiveSmallIntegerField()
+    totalEntrances = models.PositiveSmallIntegerField()
+    
+    def __str__(self):
+        return self.buildingName
